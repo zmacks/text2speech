ee2495197ca51b608ae417ed6001d73ebb1665f9
Josh Mackey <joshzpate@gmail.com>
2023-12-26 12:55:40 -0500

    Update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,22 +5,25 @@ set -euo pipefail
 # Check if env file exists
 if [ ! -f .env ] ;
 then \
-  echo "####################################################################"
-  echo "Creating .env file."
-  echo "Please edit .env file by following the instructions"
-  echo "####################################################################"
-  cp .env.local .env 
-fi 
+  echo "Creating .env file..."
+  touch .env 
+  echo "# Go to https://platform.openai.com/api-keys and 'Create new secret key' to fill this in" >> .env
+  echo "OPENAI_API_KEY=" >> .env
+  echo "####################################################################"; \
+  echo "Invalid OPENAI_API_KEY, please follow instructions in .env file" ; \
+  echo "####################################################################"; \
+exit 1; fi
 
 # Source env file 
 source .env
-echo "Sourced env file" 
 
-# Check if IDPS Secret is set
-if [ -z $LOCAL_OPENAI_SECRET ];
+# Check if secret is set
+if [ -z $OPENAI_API_KEY ];
 then \
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; \
-echo "Invalid OPENAI SECRET, please follow instructions in .env file" ; \
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; \
+echo "####################################################################"; \
+echo "Invalid OPENAI_API_KEY, please follow instructions in .env file" ; \
+echo "####################################################################"; \
 exit 1; fi
+
+echo "Setup successful!"
 
